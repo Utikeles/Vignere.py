67d34175feec170fdb434ccacc61d7e112907e12
Utikeles <142643953+Utikeles@users.noreply.github.com>
2025-02-15 13:32:30 -0500

    Create Cypher.py

---
+++ Cypher.py
@@ -0,0 +1,55 @@
+def generate_key(msg, key):
+    key = list(key)
+    if len(msg) == len(key):
+        return key
+    else:
+        for i in range(len(msg) - len(key)):
+            key.append(key[i % len(key)])
+    return "".join(key)
+
+def encrypt_vigenere(msg, key):
+    encrypted_text = []
+    key = generate_key(msg, key)
+    for i in range(len(msg)):
+        char = msg[i]
+        if char.isupper():
+            encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('A')) % 26 + ord('A'))
+        elif char.islower():
+            encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('a')) % 26 + ord('a'))
+        else:
+            encrypted_char = char
+        encrypted_text.append(encrypted_char)
+    return "".join(encrypted_text)
+
+def decrypt_vigenere(msg, key):
+    decrypted_text = []
+    key = generate_key(msg, key)
+    for i in range(len(msg)):
+        char = msg[i]
+        if char.isupper():
+            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('A'))
+        elif char.islower():
+            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('a'))
+        else:
+            decrypted_char = char
+        decrypted_text.append(decrypted_char)
+    return "".join(decrypted_text)
+
+if __name__ == "__main__":
+    while True:
+        key = str(input("Enter your key: "))
+        encryptOrDecrypt = str(input("Do you want to encrypt or decrypt? "))
+        if encryptOrDecrypt.lower()=="exit":
+            exit()
+        if encryptOrDecrypt.lower()=="encrypt":
+            textInput = str(input("Enter text to encrypt: "))
+            encrypted_text = encrypt_vigenere(textInput, key)
+            print(f"Encrypted Text: {encrypted_text}")
+            continue
+        if encryptOrDecrypt.lower()=="decrypt":
+            textInput = str(input("Enter text to decrypt: "))
+            decrypted_text = decrypt_vigenere(textInput, key)
+            print(f"Decrypted Text: {decrypted_text}")
+            continue
+        else:
+            print("Enter encrypt, decrypt, or exit.")
